e1b5c1ebaf19bc52f5cce071e15250253156241c
Andrik <masmay.pl@gmail.com>
2022-07-24 14:03:57 +0200

    final commit

--- URLSession_HW23_Andrei_Maskal.playground/Contents.swift
+++ URLSession_HW23_Andrei_Maskal.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+var sessionConfig: URLSessionConfiguration = {
+    let configuration = URLSessionConfiguration.default
+    configuration.allowsCellularAccess = false
+    configuration.waitsForConnectivity = true
+    configuration.timeoutIntervalForRequest = 15.0
+    configuration.timeoutIntervalForResource = 20.0
+    return configuration
+}()
+
+let session = URLSession(configuration: sessionConfig)
+
+func getData(url: String?) {
+    let url = URL(string: url ?? "вставьте ссылку")
+    guard let url = url else { return }
+    
+    if  !UIApplication.shared.canOpenURL(url) {
+        print("ссылка сформирована некорректно")
+    }
+    
+    session.dataTask(with: url) { data, response, error in
+        if error != nil && UIApplication.shared.canOpenURL(url)  {
+            print("DataTask error:" + "\(String(describing: error?.localizedDescription ?? ""))" + "\n")
+        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print("Status code: \(response.statusCode)")
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+            print(dataAsString ?? "")
+        }
+    }.resume()
+}
+
+getData(url: "https://emojihub.herokuapp.com/api/random")
+
